ac3761b17aa8745f84bfadb7e95576d2560c3247
jsc31994 <jsc31994@gmail.com>
2020-02-19 13:14:15 -0800

    Added the browser navigator

--- employees.php
+++ employees.php
@@ -1,4 +1,3 @@
 <?php
-
 echo "<h1>Test</h1>";
 ?>
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var employees = new Array();
 var idArray = new Array();
+var out2 = document.getElementById("navigator");
+var sBrowser,
+   sUsrAg = navigator.userAgent;
 
 function Employee(firstName, lastName, hireDate, id, department) {
    this.firstName = firstName;
@@ -9,19 +12,26 @@ function Employee(firstName, lastName, hireDate, id, department) {
    this.department = department;
 }
 
+//Creates employee only if they are not already in
+
 function createEmp() {
    var output = document.getElementById("js-output");
+
+   //creates the random unique id
    var tempId = Math.random() * 1000;
    var employeeID = tempId.toPrecision(8).replace(".", "");
 
+   //makes sure the id is truly unique
    while (idArray.includes(employeeID)) {
       var newTempId = Math.random() * 1000;
       employeeID = newTempId.toPrecision(8).replace(".", "");
    }
 
+   //id is stored in out id array
    idArray.push(employeeID);
    //    console.log(idArray[0]);
 
+   //creates variables for our values
    var fname = document.getElementById("first-name");
    var lname = document.getElementById("last-name");
    var d = new Date();
@@ -31,6 +41,8 @@ function createEmp() {
 
    for (i = 0; i < employees.length; i++) {
       var obj2 = JSON.parse(JSON.stringify(employees[i]));
+
+      //make sure our entry does not alreay exist
       if (
          fname.value === obj2.firstName &&
          lname.value === obj2.lastName &&
@@ -42,12 +54,15 @@ function createEmp() {
          counter++;
       }
    }
+   //make sure the user enters something
    if (
       (fname.value === "" || lname.value === "") &&
       counter === employees.length
    ) {
-      output.innerHTML = "<p>Please fill in the form.</p>";
-   } else if (counter === employees.length) {
+      output.innerHTML = "<p>Please fill in the form completely. </p>";
+   }
+   //creates the employee json object and sends it to our page
+   else if (counter === employees.length) {
       var newEmp = Employee(fname, lname, d, num, dep);
 
       var obj = {
@@ -58,8 +73,10 @@ function createEmp() {
          department: dep.value
       };
 
+      console.log("Our current JSON Object:");
+      console.log(obj);
       employees.push(obj);
-      console.log(JSON.stringify(employees));
+      console.log("Our list of employees ==> " + JSON.stringify(employees));
 
       output.innerHTML =
          "<p>Name: " +
@@ -75,8 +92,42 @@ function createEmp() {
          "<br>Total Employees: " +
          employees.length +
          "</p>";
+
+      showBrowser();
    }
-   console.log("Counter -> " + counter);
-   console.log("Employees array -> " + employees.length);
+   //    console.log("Counter -> " + counter);
+   //    console.log("Employees array -> " + employees.length);
 }
+console.log(employees);
 var myForm = document.getElementById("my-form");
+
+// The order matters here, and this may report false positives for unlisted browsers.
+
+function showBrowser() {
+   if (sUsrAg.indexOf("Firefox") > -1) {
+      sBrowser = "Mozilla Firefox";
+      // "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+   } else if (sUsrAg.indexOf("SamsungBrowser") > -1) {
+      sBrowser = "Samsung Internet";
+      // "Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36
+   } else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
+      sBrowser = "Opera";
+      // "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106"
+   } else if (sUsrAg.indexOf("Trident") > -1) {
+      sBrowser = "Microsoft Internet Explorer";
+      // "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; wbx 1.0.0; rv:11.0) like Gecko"
+   } else if (sUsrAg.indexOf("Edge") > -1) {
+      sBrowser = "Microsoft Edge";
+      // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+   } else if (sUsrAg.indexOf("Chrome") > -1) {
+      sBrowser = "Google Chrome or Chromium";
+      // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36"
+   } else if (sUsrAg.indexOf("Safari") > -1) {
+      sBrowser = "Apple Safari";
+      // "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
+   } else {
+      sBrowser = "unknown";
+   }
+
+   out2.innerHTML = "<p>Using: " + sBrowser + "</p>";
+}
